6471637119ecdaadb3fac069aeef18f1b3e679b6
florin <florinasca@mail.ru>
2025-05-05 19:28:40 +0300

    Initial commiot.

---
+++ src/main/java/tymleaf_app/tymleaf_app/TymleafAppApplication.java
@@ -0,0 +1,13 @@
+package tymleaf_app.tymleaf_app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TymleafAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TymleafAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/tymleaf_app/tymleaf_app/TymleafAppApplicationTests.java
@@ -0,0 +1,13 @@
+package tymleaf_app.tymleaf_app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TymleafAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
